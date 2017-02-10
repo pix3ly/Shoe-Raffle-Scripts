9a97a9f2f95d4df8f5d5f9c9c4729767039e7852
Daniël van Drunen <daniel@pixely.me>
2017-02-10 22:53:00 +0100

    Add config

---
+++ config.example.py
@@ -0,0 +1,19 @@
+name = dict(
+    first = 'John',
+    last = 'Doe'
+)
+
+birth = dict(
+    year = '1996',
+    month = '03',
+    day = '17'
+)
+
+gender = 'male'
+gmail = 'johndoe' # @gmail.com
+size = '9.5'
+street = 'PLACEHOLDER'
+postal = 'PLACEHOLDER'
+city = 'PLACEHOLDER'
+state = 'IL' # abbreviations, f.e. IL for illinois
+phone = 'PLACEHOLDER' # no spaces
--- ruvilla.py
+++ ruvilla.py
@@ -1,28 +1,31 @@
 import requests
 from random import getrandbits
+
+import config
+
 url = 'https://www.ruvilla.com/raffles/yeezy/submit.php'
 
 headers = {'User-Agent':
            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
 
-# CHANGE the fields as the comments say  
+# CHANGE the fields as the comments say
 def main(limit):
     for i in range(1, limit):
-        email = 'your_email+{}@gmail.com'.format(getrandbits(40)) # CHANGE YOUR_EMAIL to your email prefix. don't change the +{} after.
+        email = config.gmail + '+{}@gmail.com'.format(getrandbits(40)) # CHANGE YOUR_EMAIL to your email prefix. don't change the +{} after.
         payload = {
-            'firstname': '', # put your first name
-            'lastname': '', # put your last name
+            'firstname': config.name['first'], # put your first name
+            'lastname': config.name['last'], # put your last name
             'emailaddress': email, # DO NOT CHANGE
             'emailaddressverify': email, # DO NOT CHANGE
-            'birthdate': '', # PUT your birth year only, like 1995
-            'mobilephone': '', # put your number without spaces, like 1234567890
-            'homestreet': '', # put your homestreet and number
-            'homecity': '', # put your city
-            'homestate': '', # PUT YOUR STATE ABREVIATION, like IL for illinois
-            'homezip': '', # put your zipcode 
-            'gender': 'male', # just leave this alone b.
-            'shoesize': '' # put ONE shoe size, like 10, 9.5, etc.
+            'birthdate': config.dob, # PUT your birth year only, like 1995
+            'mobilephone': config.phone, # put your number without spaces, like 1234567890
+            'homestreet': config.street, # put your homestreet and number
+            'homecity': config.city, # put your city
+            'homestate': config.state, # PUT YOUR STATE ABREVIATION, like IL for illinois
+            'homezip': config.postal, # put your zipcode
+            'gender': config.gender, # just leave this alone b.
+            'shoesize': config.size # put ONE shoe size, like 10, 9.5, etc.
         }
         resp = requests.post(url, data=payload, headers=headers)
         print('{}/{} registered.'.format(i, limit))
